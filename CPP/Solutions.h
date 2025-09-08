8779a69597dfeef64eccb35f2900e7657b4219b6
ba1ram <unukh2004@gmail.com>
2025-09-08 15:49:53 +0300

    Подготовка

---
+++ CPP/Solutions.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+#include <iostream>
+
+class Solutions{
+public:
+	//1317. Convert Integer to the Sum of Two No-Zero Integers
+	std::vector<int> getNoZeroIntegers(int n) {
+		std::cout << n << std::endl;
+		return {};
+    }
+
+	
+	
+};
---
+++ CPP/main.cpp
@@ -0,0 +1,12 @@
+#include "Solutions.h"
+
+
+
+
+int main(int argc, char** argv){
+	Solutions tasks;
+	
+	tasks.getNoZeroIntegers(4);
+	
+	return 0;
+}
