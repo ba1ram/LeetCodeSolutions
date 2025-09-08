77ff1829185e1050c83f17e8ba21270e43a8a54e
ba1ram <unukh2004@gmail.com>
2025-09-08 17:50:19 +0300

    задание 1

--- CPP/Solutions.h
+++ CPP/Solutions.h
@@ -1,15 +1,46 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <string>
+#include <unordered_map>
 
 class Solutions{
 public:
+
 	//1317. Convert Integer to the Sum of Two No-Zero Integers
+	bool has_zero(int n){
+		if(n == 0) return true;
+		while(n > 0){
+			if(n % 10 == 0) return true;
+			n /= 10;
+		}
+		return false;
+	}
 	std::vector<int> getNoZeroIntegers(int n) {
-		std::cout << n << std::endl;
+		int B = 0;
+		for(int A = 1; A <= n; A++){
+			B = n - A;
+			
+			if(!has_zero(A) && !has_zero(B))
+				return {A, B};
+		}
 		return {};
     }
-
+		
+	//1. Two Sum
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+		std::unordered_map<int, int> mp;
+		
+		for(int i = 0; i < nums.size(); i++){
+			int piece = target - nums[i];
+			
+			if(mp.find(piece) != mp.end()) return {mp[piece], i};
+			mp.insert({nums[i], i});
+			
+		}
+		
+		return {};
+    }
 	
 	
 };
--- CPP/main.cpp
+++ CPP/main.cpp
@@ -6,7 +6,10 @@
 int main(int argc, char** argv){
 	Solutions tasks;
 	
-	tasks.getNoZeroIntegers(4);
+	std::vector nums = {2, 5, 5, 11};
+	for(auto i : tasks.twoSum(nums, 10)){
+		std::cout << i << std::endl;
+	}
 	
 	return 0;
 }
